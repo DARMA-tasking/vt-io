c6efb42dc7d48bed043cd625c833c6736c3415f8
Jonathan Lifflander <jliffla@sandia.gov>
2023-05-03 17:41:20 -0700

    #master: initial: add libraries and basic cmake

---
+++ cmake/load_packages.cmake
@@ -0,0 +1,18 @@
+
+# json library always included in the build
+set(JSON_BuildTests OFF)
+set(JSON_MultipleHeaders ON)
+set(JSON_LIBRARY nlohmann_json)
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/json)
+
+# fmt always included in the build
+set(FMT_LIBRARY fmt)
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/fmt)
+
+# brotli library always included in the build
+set(BROTLI_DISABLE_TESTS ON)
+# we need to disable bundled mode so it will install properly
+set(BROTLI_BUNDLED_MODE OFF)
+set(BROTLI_BUILD_PORTABLE ON)
+set(BROTLI_LIBRARY brotlicommon-static brotlienc-static brotlidec-static)
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/brotli)
